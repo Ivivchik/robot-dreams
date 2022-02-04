8e91ba766e89cddb3abd8a35c2bfca2ba0d0fc7c
High sparrow Kuhic <iv.ivchick@mail.ru>
2022-02-04 04:25:04 +0300

    added postgres hook

--- homework4/airflow/dags/load-data-from-postgresql/app/read_table.py
+++ homework4/airflow/dags/load-data-from-postgresql/app/read_table.py
@@ -1,22 +1,14 @@
 import os
-import psycopg2
-
-pg_creds = {
-    'host': '192.168.1.82',
-    'port': '5432',
-    'database': 'dshop',
-    'user': 'pguser',
-    'password': 'secret'
-}
 
 def read_table_from_db(ds, **kwargs):
 
     table_name = kwargs['table_name']
+    postgresql_hook = kwargs['postgresql_hook']
+    pg_connection = postgresql_hook.get_conn()
+    cursor = pg_connection.cursor()
     path_to_output = os.path.join('.', 'postgresql-data', table_name, ds)
-    os.makedirs(path_to_output, exist_ok=True)
-    
-    with psycopg2.connect(**pg_creds) as pg_connection:
-        cursor = pg_connection.cursor()
+
+    os.makedirs(path_to_output, exist_ok=True)    
 
     query = f"COPY (SELECT * FROM {table_name}) TO STDOUT WITH HEADER CSV DELIMITER ';'"
 
--- homework4/airflow/dags/load-data-from-postgresql/load_data_dag.py
+++ homework4/airflow/dags/load-data-from-postgresql/load_data_dag.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.join('/home/user/airflow/dags/load-data-from-postgresql/
 from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.postgres_hook import PostgresHook
 from app.read_table import read_table_from_db
 
 local_tz = pendulum.timezone("Europe/Moscow")
@@ -34,11 +35,14 @@ dag.doc_md = """
 #### Output data: ./postgresql-data/tableName/executionDate/tableName.csv
 """
 
+postgresql_hook = PostgresHook(postgres_conn_id='my_postgres_connection')
+
+
 def load_table(table_name):
     load_data = PythonOperator(
-        task_id = 'load_data',
+        task_id = f'load_data_{table_name}',
         python_callable = read_table_from_db,
-        op_kwargs={'table_name': table_name},
+        op_kwargs={'table_name': table_name, 'postgresql_hook': postgresql_hook},
         dag = dag
     )
     return load_data
